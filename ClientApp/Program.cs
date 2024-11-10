eba5b41b8c2c793db80265d14fd2d19c39c032ad
Thomas Fanara <thomas.fanara@alegeus.com>
2024-11-09 22:37:00 -0600

    added test chat completion for Ollama-phi3.5 LLM

---
+++ ClientApp/Program.cs
@@ -0,0 +1,42 @@
+﻿#pragma warning disable SKEXP0001
+#pragma warning disable SKEXP0003
+#pragma warning disable SKEXP0010
+#pragma warning disable SKEXP0011
+#pragma warning disable SKEXP0050
+#pragma warning disable SKEXP0052
+#pragma warning disable SKEXP0070
+
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+
+// Create a kernel with OpenAI chat completion
+// Warning due to the experimental state of some Semantic Kernel SDK features.
+#pragma warning disable SKEXP0070
+Kernel kernel = Kernel.CreateBuilder()
+                    .AddOllamaChatCompletion(
+                        modelId: "phi3.5",
+                        endpoint: new Uri("http://localhost:11434"))
+                    .Build();
+
+var aiChatService = kernel.GetRequiredService<IChatCompletionService>();
+var chatHistory = new ChatHistory();
+
+while (true)
+{
+    // Get user prompt and add to chat history
+    Console.WriteLine("Your prompt:");
+    var userPrompt = Console.ReadLine();
+    chatHistory.Add(new ChatMessageContent(AuthorRole.User, userPrompt));
+
+    // Stream the AI response and add to chat history
+    Console.WriteLine("AI Response:");
+    var response = "";
+    await foreach (var item in
+        aiChatService.GetStreamingChatMessageContentsAsync(chatHistory))
+    {
+        Console.Write(item.Content);
+        response += item.Content;
+    }
+    chatHistory.Add(new ChatMessageContent(AuthorRole.Assistant, response));
+    Console.WriteLine();
+}
