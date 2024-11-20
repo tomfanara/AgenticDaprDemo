e36f2a860bb723ccec6d864beacb29c4a3bc8177
Thomas Fanara <tfanara@outlook.com>
2024-11-20 11:59:58 -0600

    misc debug changes

--- Workflow.API/Features/Activities/AccountingActivity.cs
+++ Workflow.API/Features/Activities/AccountingActivity.cs
@@ -1,5 +1,8 @@
 ﻿using Dapr.Client;
 using Dapr.Workflow;
+using System.Text.Json;
+using System.Threading.Tasks.Dataflow;
+using Workflow.API.Models.Request;
 using static Workflow.API.Models.TaskChainingModels;
 
 namespace Workflow.API.Features.Activities
@@ -13,11 +16,29 @@ namespace Workflow.API.Features.Activities
             this.daprClient = daprClient;
         }
 
-        public override async Task<string> RunAsync(WorkflowActivityContext context, string steps)
+        public override async Task<string> RunAsync(WorkflowActivityContext context, string message)
         {
-           
 
-            return "hello";
+            //var httpClient = DaprClient.CreateInvokeHttpClient();
+            //Message catMessage = new Message { Messages = message };
+            //var response = await httpClient.PostAsJsonAsync<string>("http://localhost:5167/converse", message);
+
+            string value = message;
+            //if (response.IsSuccessStatusCode)
+            //{
+            //    string responseBody = await response.Content.ReadAsStringAsync();
+            //    JsonDocument jsonDocument = JsonDocument.Parse(responseBody);
+            //    JsonElement root = jsonDocument.RootElement;
+
+            //    value = root.GetProperty("conversation").GetString();
+              
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"Error: {response.StatusCode}");
+            //}
+
+            return await Task.FromResult(message);
         }
     }
 }
--- Workflow.API/Features/Workflows/TaskChainingWorkflow.cs
+++ Workflow.API/Features/Workflows/TaskChainingWorkflow.cs
@@ -20,13 +20,13 @@ public class TaskChainingWorkflow : Workflow<string, string>
         };
 
         try
-        {
-            Console.WriteLine("hello");
+        {           
             var result1 = await context.CallActivityAsync<string>("AccountingActivity", prompt, retryOptions);
-            var result2 = await context.CallActivityAsync<string>("InventoryActivity", prompt, retryOptions);
-            var result3 = await context.CallActivityAsync<string>("SalesActivity", prompt, retryOptions);
-            Console.WriteLine("hello");
-            return string.Join(", ", result1, result2, result3);
+            //var result2 = await context.CallActivityAsync<string>("InventoryActivity", prompt, retryOptions);
+            //var result3 = await context.CallActivityAsync<string>("SalesActivity", prompt, retryOptions);
+            //Console.WriteLine("hello");
+            //return string.Join(", ", result1, result2, result3);
+            return result1;
            
         }
         catch (TaskFailedException) // Task failures are surfaced as TaskFailedException
